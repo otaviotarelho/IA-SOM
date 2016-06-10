f01b1e561f10edb19ed5a2bcf95be828df097eb8
Otavio Tarelho <otarelho@ucdavis.edu>
2016-06-10 11:32:34 -0300

    Changing algorithm based on Toronto University PDF

--- src/som/SOM.java
+++ src/som/SOM.java
@@ -22,16 +22,16 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+
 /**
  *
  * @author otaviotarelho
  */
-
 public class SOM {
 
     private static double[][] w = new double[4][2];
-    private static double trainingRate = 0.5;
-    private static double trainingActualRate = 0.5;
+    private static final double LEARNING = 0.5; //Learning rate at it = 0;
+    private static double trainingActualRate = 0.5; //decay with iterations;
     private static double[][] _td
             = {
                 {82.0, 1.80},
@@ -57,25 +57,37 @@ public class SOM {
                 {140.0, 2.00},
                 {130.0, 1.70},
                 {125.0, 1.80},
-                {120.0, 1.60}};
+                {120.0, 1.60}
+            };
 
-    private static int[][] neuron = {{0, 1}, {2, 3}}; // neuronios e sua distancia
-    private static int TIME = 1000 + (500 * w.length); // total maximo de iterações
-    private static int FTEMPO = 10 * TIME;
-    private static int[][] tdBMU = new int[25][3]; // Neuronio vencedor , X, Y;
-    private static final int DIMENSION = 2;
+    private static int[][] neuron = {{0, 1}, {2, 3}}; // Neurons
+    private static int TIME = 1000 + (500 * w.length); // Maximum iterations
+    private static int FTEMPO = 100000; // Time Constant
+    private static int[][] tdBMU = new int[25][3]; // Winner neuron, and X, Y of neuron 
+    private static final int DIMENSION = 2; // Neuron lattice dimension
+    private static final double Neighborhood = 1.0; // Neighborhood Size at Time 0
+    private static double NSize = 1.0; // Neighborhood Size chenges when time changes
 
     public static void weightsInit() {
+
         double[][] newWeights = new double[4][2];
         double verify;
         Random randomno = new Random();
+
         for (double[] newWeight : newWeights) {
+
             for (int j = 0; j < newWeights[0].length; j++) {
+
                 verify = randomno.nextDouble();
+
                 if (verify >= -0.5 && verify <= 0.5) {
+
                     newWeight[j] = verify;
+
                 } else {
+
                     j--;
+
                 }
             }
         }
@@ -83,7 +95,7 @@ public class SOM {
     }
 
     public static void BMU(int i) {
-        double menor = 0.0;
+        double menor = 0.0; //small distance
 
         for (int j = 0; j < w.length; j++) {
 
@@ -92,12 +104,16 @@ public class SOM {
             distance = euclideanDistance(_td[i][0], _td[i][1], w[j][0], w[j][1]);
 
             if (j == 0) {
+
                 menor = distance;
                 tdBMU[i][0] = j;
+
             } // end if first weight
             else if (distance < menor) {
+
                 menor = distance;
                 tdBMU[i][0] = j;
+
             } // else weights
         }
 
@@ -131,73 +147,96 @@ public class SOM {
         return Math.sqrt(Math.pow(x1 - y1, 2)) + Math.sqrt(Math.pow(x2 - y2, 2));
     }
 
-    public static void updateWeights(int i, int d) {
+    public static void updateWeights(int it, int d) {
+
         for (int j = 0; j < 2; j++) {
-            w[i][j] += trainingRate * (_td[d][j] - w[i][j]);
+
+            w[it][j] = w[it][j] + trainingActualRate * (_td[d][j] - w[it][j]);
+
         }
     }
 
     public static void updateWeightsNeighbors(int i, int d) {
+        double influenceRate;
+
         for (int j = 0; j < 2; j++) {
-            w[i][j] += trainingRate * trainingActualRate * (_td[d][j] - w[i][j]);
+
+            influenceRate = influenceRate(d, _td[d][0], _td[d][0], w[i][j], w[i][j]);
+            w[i][j] = w[i][j] + trainingActualRate * influenceRate * (_td[d][j] - w[i][j]);
+
         }
     }
 
-    public static void changeFactor(int it) {
-        trainingActualRate = trainingRate * Math.exp(-(it / FTEMPO));
+    public static void ChangeLearning(int it) {
+
+        trainingActualRate = LEARNING * Math.exp((-1.0 * it) / FTEMPO);
+
+    }
+
+    public static void ChangeRNeighbohood(int it) {
+
+        NSize = Neighborhood * Math.exp((-1.0 * it) / FTEMPO);
+
+    }
+
+    public static double influenceRate(int it, double x, double x1, double y, double y1) {
+
+        double distance;
+        distance = euclideanDistance(x, y, x1, y1);
+
+        return Math.exp(-1.0 * Math.pow(distance, 2) / (2.0 * Math.pow(NSize, 2)));
+
     }
 
     public static void main(String[] args) throws IOException {
         int e = 0;
-        weightsInit();
 
-        System.out.println(Arrays.toString(w[0]));
-        System.out.println(Arrays.toString(w[1]));
-        System.out.println(Arrays.toString(w[2]));
-        System.out.println(Arrays.toString(w[3]));
+        weightsInit();
 
         while (e < TIME) {
 
             for (int i = 0; i < _td.length; i++) {
+
                 BMU(i);
                 setCoordinates(i);
+
             }
-            changeFactor(e);
+
+            ChangeRNeighbohood(e);
+            ChangeLearning(e);
             e++;
         }
 
-        System.out.println(Arrays.toString(w[0]));
-        System.out.println(Arrays.toString(w[1]));
-        System.out.println(Arrays.toString(w[2]));
-        System.out.println(Arrays.toString(w[3]));
-
         int i = 0;
         while (i < 25) {
-            System.out.println("Vencedor:" + (tdBMU[i][0] + 1) + " - X: " + tdBMU[i][1] + " Y: " + tdBMU[i][2]);
+
+            System.out.println("Vencedor Final:" + (tdBMU[i][0] + 1) + " - X: " + tdBMU[i][1] + " Y: " + tdBMU[i][2]);
             i++;
+
         }
-        XYSeries series = new XYSeries("Neuronio");
-        XYSeriesCollection ds = new XYSeriesCollection();
-        
-        for(int f = 0; f < tdBMU[0].length; f++){
-            ds.addValue(tdBMU[f][1], "Neuronio " + tdBMU[f][0], "" +tdBMU[f][2]);
-            series.add(f, f);
-        }
-        
-        JFreeChart grafico = ChartFactory.createLineChart("Self Organizing Maps", "X", "Y", ds, PlotOrientation.VERTICAL, true, true, false);
-        JFreeChart g = ChartFactory.createScatterPlot("Self Organizing Maps", "X", "Y", ds, PlotOrientation.VERTICAL, true, true, false);
-        
-        JFrame frame = new JFrame("IMC SOM");
-        frame.add(getPanel(grafico));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        
+
+//        XYSeries series = new XYSeries("Neuronio");
+//        XYSeriesCollection ds = new XYSeriesCollection();
+//        
+//        for(int f = 0; f < tdBMU[0].length; f++){
+//            ds.addValue(tdBMU[f][1], "Neuronio " + tdBMU[f][0], "" +tdBMU[f][2]);
+//            series.add(f, f);
+//        }
+//        
+//        JFreeChart grafico = ChartFactory.createLineChart("Self Organizing Maps", "X", "Y", ds, PlotOrientation.VERTICAL, true, true, false);
+//        JFreeChart g = ChartFactory.createScatterPlot("Self Organizing Maps", "X", "Y", ds, PlotOrientation.VERTICAL, true, true, false);
+//        
+//        JFrame frame = new JFrame("IMC SOM");
+//        frame.add(getPanel(grafico));
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
     }
+
     public static JPanel getPanel(JFreeChart grafico) {
         return new ChartPanel(grafico);
     }
-    
+
     public static void setCoordinates(int it) {
         for (int i = 0; i < DIMENSION; i++) {
             for (int j = 0; j < DIMENSION; j++) {
